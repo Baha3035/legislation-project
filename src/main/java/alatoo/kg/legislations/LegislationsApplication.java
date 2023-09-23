4250b33709a072c74e19a18bd40cb75edc013ea0
Baha3035 <batmaaanpls@gmail.com>
2023-09-23 17:02:00 +0600

    first commit

---
+++ src/main/java/alatoo/kg/legislations/LegislationsApplication.java
@@ -0,0 +1,13 @@
+package alatoo.kg.legislations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegislationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegislationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alatoo/kg/legislations/LegislationsApplicationTests.java
@@ -0,0 +1,13 @@
+package alatoo.kg.legislations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegislationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
